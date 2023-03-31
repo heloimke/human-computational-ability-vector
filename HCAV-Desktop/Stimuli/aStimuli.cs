26273870e642b584fc32e1783ac071583a774cf9
Ike <heloimke@gmail.com>
2023-03-31 15:25:06 +0800

    Extra Stimuli Functions
    Added a few more functions to the aStimuli and gStimuli classes exposing functionality in MonoGame

--- HCAV-Desktop/Stimuli/aStimuli.cs
+++ HCAV-Desktop/Stimuli/aStimuli.cs
@@ -16,9 +16,26 @@ public class aStimuli : Stimuli<SoundEffect>
         }
     }
 
+    private AudioEmitter _instance_emitter;
+    public AudioEmitter instance_emitter => _instance_emitter;
+    public AudioEmitter instance_emitter_safe {
+        get {
+            if (_instance_emitter == null) _instance_emitter = new AudioEmitter();
+            return _instance_emitter;
+        }
+    }
+
+
     public void SetPitch(float pitch) => instance_safe.Pitch = MathHelper.Clamp(pitch, -1, 1);
     public void SetVolume(float volume) => instance_safe.Volume = MathHelper.Clamp(volume, 0, 1);
     public void SetPan(float pan) => instance_safe.Pan = MathHelper.Clamp(pan, -1, 1);
+
+    public void Set3DPos(AudioListener listener, AudioEmitter emitter) => instance_safe.Apply3D(listener, emitter);
+    public void Set3DPos(AudioListener listener, Vector3 emission)
+    {
+        instance_emitter_safe.Position = emission;
+        instance_safe.Apply3D(listener, _instance_emitter);
+    }
 
     public aStimuli(ContentManager ContainingLibrary, string AssetName, string StimuliCode, bool DefaultInstance = true, bool AutoLoad = true) : base (ContainingLibrary, StimulusType.Audio, AssetName, StimuliCode, AutoLoad)
     {
--- HCAV-Desktop/Stimuli/gStimuli.cs
+++ HCAV-Desktop/Stimuli/gStimuli.cs
@@ -27,4 +27,16 @@ public class gStimuli : Stimuli<Texture2D>
     public void Draw(ref SpriteBatch batch, Vector2 position, Vector2 scale, float rotation = 0, float layerDepth = 0, SpriteEffects effect = SpriteEffects.None) => Draw(ref batch, this.Origin, position, scale, rotation, layerDepth, effect);
     public void Draw(ref SpriteBatch batch, Vector2 position, float scale, float rotation = 0, float layerDepth = 0, SpriteEffects effect  = SpriteEffects.None) => Draw(ref batch, this.Origin, position, Vector2.One * scale, rotation, layerDepth, effect);
     public void Draw(ref SpriteBatch batch, Vector2 origin, Vector2 position, float scale, float rotation = 0, float layerDepth = 0, SpriteEffects effect = SpriteEffects.None) => Draw(ref batch, origin, position, Vector2.One * scale, rotation, layerDepth, effect);
+
+    public void Draw(ref SpriteBatch batch, Rectangle destination) => batch.Draw(this.content, destination, Color.White);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, Color color) => batch.Draw(this.content, destination, color);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, float rotation, float layerDepth = 0) => batch.Draw(this.content, destination, null, Color.White, rotation, Vector2.Zero, SpriteEffects.None, layerDepth);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, float rotation, Color color, float layerDepth = 0) => batch.Draw(this.content, destination, null, color, rotation, Vector2.Zero, SpriteEffects.None, layerDepth);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, float rotation, Vector2 rotOrigin, float layerDepth = 0) => batch.Draw(this.content, destination, null, Color.White, rotation, rotOrigin, SpriteEffects.None, layerDepth);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, Color color, float rotation, Vector2 rotOrigin, float layerDepth = 0) => batch.Draw(this.content, destination, null, color, rotation, rotOrigin, SpriteEffects.None, layerDepth);
+    
+    public void Draw(ref SpriteBatch batch, Rectangle destination, Rectangle source, float rotation = 0, float layerDepth = 0) => batch.Draw(this.content, destination, source, Color.White, rotation, Vector2.Zero, SpriteEffects.None, layerDepth);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, Rectangle source, Color color, float rotation = 0, float layerDepth = 0) => batch.Draw(this.content, destination, source, color, rotation, Vector2.Zero, SpriteEffects.None, layerDepth);
+    public void Draw(ref SpriteBatch batch, Rectangle destination, Rectangle source, Color color, float rotation, Vector2 rotOrigin, float layerDepth = 0) => batch.Draw(this.content, destination, source, color, rotation, rotOrigin, SpriteEffects.None, layerDepth);
+    
 }
